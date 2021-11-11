e8bff005183057bac708ff8e2308850315fc0fb1
Fatemeh Saadabady <fatemehsaadabady.f.2002@gmail.com>
2021-11-12 00:12:07 +0330

    END OF PHASE#1

--- src/forceps.cpp
+++ src/forceps.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-forceps::forceps(string name, knife_size(medium))             // کانستراکتور
+forceps::forceps(string name, forceps_size(medium))             // کانستراکتور
 {
      set_name(name);
-    
 }
 
 void forceps::set_name(string n)                     // توابع ست و گت
